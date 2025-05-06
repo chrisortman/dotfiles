580efc7193b790835d744b87a7b7084ac8a95a01
Chris Ortman <chris-ortman@uiowa.edu>
2025-05-06 10:17:46 -0500

    Can't depend on system updates for this because cross platform

--- mise-tasks/install-vim-plug.sh
+++ mise-tasks/install-vim-plug.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 #MISE description="Installs vim plugin manager Plug"
-#MISE depends=["install-required-packages"]
 
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
