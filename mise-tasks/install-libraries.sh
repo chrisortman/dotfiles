b998ed73612e1c49a2b149a0885395979404df51
Chris Ortman <chris-ortman@uiowa.edu>
2025-08-07 07:14:33 -0500

    Include mysql
    need if going to do development on rails core

--- mise-tasks/install-libraries.sh
+++ mise-tasks/install-libraries.sh
@@ -8,4 +8,5 @@ sudo apt install -y \
   libssl-dev libreadline-dev zlib1g-dev libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev libjemalloc2 \
   libvips imagemagick libmagickwand-dev mupdf mupdf-tools gir1.2-gtop-2.0 gir1.2-clutter-1.0 \
   redis-tools sqlite3 libsqlite3-0 libpq-dev postgresql-client postgresql-client-common \
+  libmysqlclient-dev \
   unixodbc-dev msodbcsql18 freetds
