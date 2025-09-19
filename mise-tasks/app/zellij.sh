85e2ab6b27066b570ad6d2d33a8249f0c00f1df0
Chris Ortman <chris-ortman@uiowa.edu>
2025-09-19 14:22:29 -0500

    Move zellij to app and fix broken task for fastfetch

--- mise-tasks/app/zellij.sh
+++ mise-tasks/app/zellij.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#MISE description="Installs zellij multiplexer"
+#MISE depends=["system-update"]
+
 cd /tmp
 wget -O zellij.tar.gz "https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz"
 tar -xf zellij.tar.gz zellij
