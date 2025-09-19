f41ef67d3a20e087c760d55d54ee51bbb48342ba
Chris Ortman <chris-ortman@uiowa.edu>
2025-09-19 16:49:34 -0500

    Remove fzf from required tools so it can be installed with mise (newer version)

--- mise-tasks/install/tools.sh
+++ mise-tasks/install/tools.sh
@@ -7,11 +7,6 @@
 #
 sudo apt install -y stow ripgrep bat eza zoxide btop apache2-utils fd-find tldr tmux cloc man
 
-# Ubuntu has a really old fzf so we install it using mise instead. the biggest
-# difference compatability wise is that presence of a command for shell integration
-# `fzf --zsh` which is not present in the version that ships with ubuntu
-mise use -g fzf
-
 mkdir -p ~/.local/bin
 ln -sf /usr/bin/batcat ~/.local/bin/bat
 ln -sf $(which fdfind) ~/.local/bin/fd
