745ebe68cbf5d73c6503c2734079f9c7b8df2052
Chris Ortman <chris-ortman@uiowa.edu>
2026-03-09 13:49:28 -0500

    Add task to install atlassian cli on macos

---
+++ mise-tasks/macOS/app/jiracli.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+
+#MISE description="Installs Jira CLI client"
+
+# https://developer.atlassian.com/cloud/acli/guides/install-acli/
+
+brew tap atlassian/homebrew-acli
+brew install acli
+
